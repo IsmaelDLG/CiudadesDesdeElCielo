06702ea4f3bb3b442f05ef12ba33d251413c5048
IsmaelDLG <ismadlg.works@gmail.com>
2020-05-19 00:29:33 +0200

    main page and crawler app

--- CiudadesDesdeElCielo/settings.py
+++ CiudadesDesdeElCielo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'crawlers.apps.CrawlersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
